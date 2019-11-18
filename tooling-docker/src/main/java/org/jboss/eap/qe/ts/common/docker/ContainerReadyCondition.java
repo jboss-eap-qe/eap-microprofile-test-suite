4b1b16c1e96f601becbef97b9e71b2a4971bfdab
mnovak <mnovak@redhat.com>
2019-11-18 08:56:12 +0100

    Provide fail-fast mechanism for starting docker container - fixes https://github.com/jboss-eap-qe/eap-microprofile-test-suite/issues/28

--- tooling-docker/src/main/java/org/jboss/eap/qe/ts/common/docker/ContainerReadyCondition.java
+++ tooling-docker/src/main/java/org/jboss/eap/qe/ts/common/docker/ContainerReadyCondition.java
@@ -1,23 +1,7 @@
 package org.jboss.eap.qe.ts.common.docker;
 
-import java.util.concurrent.CompletableFuture;
-
 @FunctionalInterface
 public interface ContainerReadyCondition {
-
-    /**
-     * Waits until isReady() returns true otherwise exception is thrown.
-     * <p>
-     * Usually this is checked by checking exposed port or calling health check inside docker container.
-     */
-    default void waitUntilReady(long timeoutInMillis) throws Exception {
-        long startTime = System.currentTimeMillis();
-        while (!(new CompletableFuture().supplyAsync(() -> isReady())).get()) {
-            if (System.currentTimeMillis() - startTime > timeoutInMillis) {
-                throw new Exception("Container was not ready in timeout : " + (timeoutInMillis / 1000) + " s");
-            }
-        }
-    }
 
     /**
      * Returns true if container is ready. Usually this checked by calling health check on running container
