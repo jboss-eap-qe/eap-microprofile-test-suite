254dbe83384c1828f92cae7260afb6c43da068a1
mnovak <mnovak@redhat.com>
2019-12-05 09:48:25 +0100

    Add convienience method for retrieving Creaper OnlineManagementClient

--- tooling-server-configuration/src/test/java/org/jboss/eap/qe/microprofile/tooling/server/configuration/CreaperManagementClientTest.java
+++ tooling-server-configuration/src/test/java/org/jboss/eap/qe/microprofile/tooling/server/configuration/CreaperManagementClientTest.java
@@ -1,5 +1,8 @@
 package org.jboss.eap.qe.microprofile.tooling.server.configuration;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.eap.qe.microprofile.tooling.server.configuration.arquillian.ArquillianContainerProperties;
 import org.jboss.eap.qe.microprofile.tooling.server.configuration.arquillian.ArquillianDescriptorWrapper;
@@ -9,21 +12,30 @@ import org.junit.runner.RunWith;
 import org.wildfly.extras.creaper.core.online.OnlineManagementClient;
 import org.wildfly.extras.creaper.core.online.operations.admin.Administration;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
-
 /**
  * Tests Creaper integration for CLI operations.
  * <p>
- * {@link ManagementClientProvider} uses {@link ArquillianContainerProperties} to retrieve default configuration from Arquillian context
+ * {@link ManagementClientProvider} uses {@link ArquillianContainerProperties} to retrieve default configuration from Arquillian
+ * context
  */
 @RunWith(Arquillian.class)
 public class CreaperManagementClientTest {
 
-    static ArquillianContainerProperties arquillianContainerProperties = new ArquillianContainerProperties(ArquillianDescriptorWrapper.getArquillianDescriptor());
+    static ArquillianContainerProperties arquillianContainerProperties = new ArquillianContainerProperties(
+            ArquillianDescriptorWrapper.getArquillianDescriptor());
 
     @Test
-    public void testReloadCommand() throws IOException, ConfigurationException, TimeoutException, InterruptedException {
+    public void testReloadCommandWithDefaultArquillianProperties()
+            throws IOException, ConfigurationException, TimeoutException, InterruptedException {
+        try (OnlineManagementClient client = ManagementClientProvider.onlineStandalone()) {
+            Administration admin = new Administration(client);
+            admin.reload();
+        }
+    }
+
+    @Test
+    public void testReloadCommandWithProvidedArquillianContainerProperties()
+            throws IOException, ConfigurationException, TimeoutException, InterruptedException {
         try (OnlineManagementClient client = ManagementClientProvider.onlineStandalone(arquillianContainerProperties)) {
             Administration admin = new Administration(client);
             admin.reload();
