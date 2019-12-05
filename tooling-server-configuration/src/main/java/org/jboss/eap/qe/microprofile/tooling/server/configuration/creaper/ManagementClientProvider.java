b66dba9ad2cf0d20f6f0e52955aa2fc5031aa6d5
mnovak <mnovak@redhat.com>
2019-12-05 09:48:25 +0100

    Add convienience method for retrieving Creaper OnlineManagementClient

--- tooling-server-configuration/src/main/java/org/jboss/eap/qe/microprofile/tooling/server/configuration/creaper/ManagementClientProvider.java
+++ tooling-server-configuration/src/main/java/org/jboss/eap/qe/microprofile/tooling/server/configuration/creaper/ManagementClientProvider.java
@@ -1,6 +1,7 @@
 package org.jboss.eap.qe.microprofile.tooling.server.configuration.creaper;
 
 import org.jboss.eap.qe.microprofile.tooling.server.configuration.arquillian.ArquillianContainerProperties;
+import org.jboss.eap.qe.microprofile.tooling.server.configuration.arquillian.ArquillianDescriptorWrapper;
 import org.wildfly.extras.creaper.core.ManagementClient;
 import org.wildfly.extras.creaper.core.online.OnlineManagementClient;
 import org.wildfly.extras.creaper.core.online.OnlineOptions;
@@ -12,6 +13,18 @@ import org.jboss.eap.qe.microprofile.tooling.server.configuration.ConfigurationE
 public class ManagementClientProvider {
 
     private ManagementClientProvider() {
+    }
+
+    /**
+     * Creates {@link OnlineManagementClient} for <b>standalone</b> mode, based on {@link ArquillianContainerProperties}
+     * obtained from {@link ArquillianDescriptorWrapper}
+     *
+     * @return Initialized {@link OnlineManagementClient} instance, don't forget to close it
+     * @throws ConfigurationException Wraps exceptions thrown by the internal operation executed by
+     *                                {@link ArquillianContainerProperties} API
+     */
+    public static OnlineManagementClient onlineStandalone() throws ConfigurationException {
+        return onlineStandalone(new ArquillianContainerProperties(ArquillianDescriptorWrapper.getArquillianDescriptor()));
     }
 
     /**
