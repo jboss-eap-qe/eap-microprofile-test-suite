47fd22ef211bd871577aa8cb53018b657843b87e
Jan Kašík <jkasik@redhat.com>
2019-12-19 14:52:46 +0100

    Basic is gone fixing #116

--- microprofile-jwt/src/test/java/org/jboss/eap/qe/microprofile/jwt/cdi/ActivationCompatibilityTest.java
+++ microprofile-jwt/src/test/java/org/jboss/eap/qe/microprofile/jwt/cdi/ActivationCompatibilityTest.java
@@ -8,7 +8,7 @@ import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.eap.qe.microprofile.jwt.auth.tool.JsonWebToken;
 import org.jboss.eap.qe.microprofile.jwt.auth.tool.JwtHelper;
 import org.jboss.eap.qe.microprofile.jwt.auth.tool.RsaKeyTool;
-import org.jboss.eap.qe.microprofile.jwt.testapp.jaxrs.JaxRsBasicEndpoint;
+import org.jboss.eap.qe.microprofile.jwt.testapp.jaxrs.SecuredJaxRsEndpoint;
 import org.jboss.eap.qe.microprofile.jwt.testapp.jaxrs.JaxRsTestApplication;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -27,13 +27,13 @@ import static org.hamcrest.CoreMatchers.equalTo;
  * Just verify a raw token value can be injected into a variable in an application class a compare it has expected value
  */
 @RunWith(Arquillian.class)
-public class BasicCdiTest {
+public class ActivationCompatibilityTest {
 
     private static RsaKeyTool keyTool;
 
     @BeforeClass
     public static void beforeClass() throws URISyntaxException {
-        final URL privateKeyUrl = BasicCdiTest.class.getClassLoader().getResource("pki/key.private.pkcs8.pem");
+        final URL privateKeyUrl = ActivationCompatibilityTest.class.getClassLoader().getResource("pki/key.private.pkcs8.pem");
         if (privateKeyUrl == null) {
             throw new IllegalStateException("Private key wasn't found in resources!");
         }
@@ -43,10 +43,10 @@ public class BasicCdiTest {
     @Deployment
     public static Archive<?> createDeployment() {
         return ShrinkWrap.create(WebArchive.class)
-                .addClass(JaxRsBasicEndpoint.class)
+                .addClass(SecuredJaxRsEndpoint.class)
                 .addClass(JaxRsTestApplication.class)
-                .addAsManifestResource(BasicCdiTest.class.getClassLoader().getResource("mp-config-basic.properties"), "microprofile-config.properties")
-                .addAsManifestResource(BasicCdiTest.class.getClassLoader().getResource("pki/key.public.pem"), "key.public.pem");
+                .addAsManifestResource(ActivationCompatibilityTest.class.getClassLoader().getResource("mp-config-basic.properties"), "microprofile-config.properties")
+                .addAsManifestResource(ActivationCompatibilityTest.class.getClassLoader().getResource("pki/key.public.pem"), "key.public.pem");
     }
 
     /**
@@ -60,7 +60,7 @@ public class BasicCdiTest {
         JsonWebToken token = new JwtHelper(keyTool, "issuer").generateProperSignedJwt();
 
         RestAssured.given().header("Authorization", "Bearer " + token.getRawValue())
-                .when().get(url.toExternalForm() + "basic-endpoint")
+                .when().get(url.toExternalForm() + "secured-endpoint")
                 .then().body(equalTo(token.getRawValue()));
     }
 
