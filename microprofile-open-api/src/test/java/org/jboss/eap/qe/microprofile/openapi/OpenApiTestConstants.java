58a731c05443af208b1195ed694282a8aa43a7f9
Fabio Burzigotti <fburzigo@redhat.com>
2019-12-20 12:10:26 +0100

    MP OpenAPI - Adding security tests

--- microprofile-open-api/src/test/java/org/jboss/eap/qe/microprofile/openapi/OpenApiTestConstants.java
+++ microprofile-open-api/src/test/java/org/jboss/eap/qe/microprofile/openapi/OpenApiTestConstants.java
@@ -6,4 +6,5 @@ package org.jboss.eap.qe.microprofile.openapi;
 public class OpenApiTestConstants {
     public final static String DEFAULT_HOST_NAME = "localhost";
     public final static String DEFAULT_ENDPOINT_PORT = "8080";
+    public final static String DEFAULT_ENDPOINT_HTTPS_PORT = "8443";
 }
