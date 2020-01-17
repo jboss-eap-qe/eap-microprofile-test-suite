ab0f55ea6f8626d659eb84670c2bfec774253572
Jan Kašík <jkasik@redhat.com>
2020-01-17 13:49:44 +0100

    Added high level Keycloak integration scenario for JWT

---
+++ microprofile-jwt/src/test/java/org/jboss/eap/qe/microprofile/jwt/keycloak/KeycloakConfigurator.java
@@ -0,0 +1,237 @@
+package org.jboss.eap.qe.microprofile.jwt.keycloak;
+
+import static io.restassured.RestAssured.given;
+
+import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
+import org.jboss.eap.qe.microprofile.jwt.testapp.Roles;
+import org.junit.rules.ExternalResource;
+
+/**
+ * Keycloak configurator specifically for JWT purposes. It configures a realm, a client and then allows adding users and
+ * generating JWTs for them.
+ */
+//See https://medium.com/@bcarunmail/securing-rest-api-using-keycloak-and-spring-oauth2-6ddf3a1efcc2
+public class KeycloakConfigurator extends ExternalResource {
+
+    private final String realmName;
+
+    private final String clientId;
+
+    private final String adminUsername;
+    private final String adminPassword;
+
+    private final URL baseApiUrl;
+
+    private String rawAdminToken;
+
+    @Override
+    public void before() {
+        rawAdminToken = authorizeAndObtainRawJwtForAdminInterface(adminUsername, adminPassword);
+        createRealmOnKeycloak(this.realmName);
+        createClientOnKeycloak(this.realmName, this.clientId);
+        createClientRole(this.realmName, this.clientId, "USER");
+    }
+
+    @Override
+    public void after() {
+        //do nothing
+    }
+
+    private String authorizeAndObtainRawJwtForAdminInterface(final String username, final String password) {
+        final String jsonResponse = given().header("Content-Type", "application/x-www-form-urlencoded")
+                .param("username", username)
+                .param("password", password)
+                .param("grant_type", "password")
+                .param("client_id", "admin-cli")
+                .post(this.baseApiUrl.toExternalForm() + "/realms/master/protocol/openid-connect/token")
+                .body().asString();
+        return extractRawTokenFromJson(jsonResponse);
+    }
+
+    private void createRealmOnKeycloak(final String realmName) {
+        given().header("Authorization", "Bearer " + this.rawAdminToken)
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .body(Json.createObjectBuilder()
+                        .add("realm", realmName)
+                        .add("displayName", realmName)
+                        .add("enabled", true)
+                        .build().toString())
+                .post(this.baseApiUrl + "/admin/realms")
+                .then()
+                .statusCode(201);
+    }
+
+    private void createClientOnKeycloak(final String realmName, final String clientId) {
+        given().header("Authorization", "Bearer " + this.rawAdminToken)
+                .header("Content-Type", "application/json")
+                .body(Json.createObjectBuilder()
+                        .add("id", clientId)
+                        .add("clientId", clientId)
+                        .add("protocol", "openid-connect")
+                        .add("redirectUris", Json.createArrayBuilder().add("http://localhost:8085")).build().toString())
+                .post(this.baseApiUrl.toExternalForm() + "/admin/realms/" + realmName + "/clients")
+                .then()
+                .statusCode(201);
+    }
+
+    private void createClientRole(String realmName, String clientId, String roleName) {
+        given().header("Authorization", "Bearer " + this.rawAdminToken)
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .body(Json.createObjectBuilder()
+                        .add("name", roleName)
+                        .build().toString())
+                .post(this.baseApiUrl.toExternalForm() + "/admin/realms/" + realmName + "/clients/" + clientId + "/roles")
+                .then()
+                .statusCode(201);
+    }
+
+    /**
+     * Create new user on Keycloak server. Realm name and client ID are pre-defined.
+     * 
+     * @param username a username
+     * @param password a password
+     */
+    public void addUser(final String username, final String password) {
+        given().header("Authorization", "Bearer " + this.rawAdminToken)
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .body(Json.createObjectBuilder()
+                        .add("username", username)
+                        .add("enabled", true)
+                        .add("credentials", Json.createArrayBuilder()
+                                .add(Json.createObjectBuilder()
+                                        .add("temporary", false)
+                                        .add("type", "password")
+                                        .add("value", password)))
+                        .add("clientRoles",
+                                Json.createObjectBuilder().add(this.clientId, Json.createArrayBuilder().add("USER")))
+                        .add("groups", Json.createArrayBuilder().add(Roles.MONITOR))
+                        .build().toString())
+                .post(this.baseApiUrl + "/admin/realms/" + this.realmName + "/users")
+                .then()
+                .statusCode(201);
+    }
+
+    /**
+     * Generate raw JWT for a user
+     * 
+     * @param username a username
+     * @param password a matching password
+     * @return a raw JWT
+     */
+    public String getRawJwtFromKeyCloak(final String username, final String password) {
+        return extractRawTokenFromJson(given().header("Content-Type", "application/x-www-form-urlencoded")
+                .param("grant_type", "password")
+                .param("username", username)
+                .param("password", password)
+                .param("client_id", this.clientId)
+                .post(this.baseApiUrl.toExternalForm() + "/realms/" + this.realmName + "/protocol/openid-connect/token")
+                .body()
+                .asString());
+    }
+
+    private String extractRawTokenFromJson(String jsonResponse) {
+        final JsonReader reader = Json.createReader(new StringReader(jsonResponse));
+        final JsonObject jsonTokenObject = reader.readObject();
+        reader.close();
+        return jsonTokenObject.getString("access_token");
+    }
+
+    public KeycloakConfigurator(Builder builder) {
+        this.realmName = builder.realmName;
+        this.clientId = builder.clientId;
+        this.adminPassword = builder.adminPassword;
+        this.adminUsername = builder.adminUsername;
+        try {
+            this.baseApiUrl = new URL("http", builder.keycloakBindAddress, builder.keycloakHttpPort, "/auth");
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("Malformed URL!", e);
+        }
+    }
+
+    public static final class Builder {
+
+        private final String realmName;
+
+        private String clientId;
+
+        private String adminUsername;
+        private String adminPassword;
+
+        private int keycloakHttpPort;
+        private String keycloakBindAddress;
+
+        public Builder(final String realmName) {
+            this.realmName = realmName;
+        }
+
+        /**
+         * Set ID of keycloak client which will be used for both client ID and its name
+         * 
+         * @param clientId client ID
+         * @return instance of this builder
+         */
+        public Builder clientId(final String clientId) {
+            this.clientId = clientId;
+            return this;
+        }
+
+        /**
+         * Set username for Keycloak administrator
+         * 
+         * @param adminUsername a username
+         * @return instance of this builder
+         */
+        public Builder adminUsername(final String adminUsername) {
+            this.adminUsername = adminUsername;
+            return this;
+        }
+
+        /**
+         * Set password for Keycloak administrator
+         * 
+         * @param adminPassword a adminPassword
+         * @return instance of this builder
+         */
+        public Builder adminPassword(final String adminPassword) {
+            this.adminPassword = adminPassword;
+            return this;
+        }
+
+        /**
+         * A hostname on which the Keycloak instance is available
+         * 
+         * @param keycloakBindAddress a hostname
+         * @return instance of this builder
+         */
+        public Builder keycloakBindAddress(final String keycloakBindAddress) {
+            this.keycloakBindAddress = keycloakBindAddress;
+            return this;
+        }
+
+        /**
+         * A port on which the Keycloak instance is available
+         * 
+         * @param keycloakHttpPort a port
+         * @return instance of this builder
+         */
+        public Builder keycloakHttpPort(final int keycloakHttpPort) {
+            this.keycloakHttpPort = keycloakHttpPort;
+            return this;
+        }
+
+        public KeycloakConfigurator build() {
+            return new KeycloakConfigurator(this);
+        }
+
+    }
+}
---
+++ microprofile-jwt/src/test/java/org/jboss/eap/qe/microprofile/jwt/keycloak/KeycloakIntegrationHighLevelScenarioTest.java
@@ -0,0 +1,122 @@
+package org.jboss.eap.qe.microprofile.jwt.keycloak;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.eap.qe.microprofile.jwt.testapp.Endpoints;
+import org.jboss.eap.qe.microprofile.jwt.testapp.jaxrs.JaxRsTestApplication;
+import org.jboss.eap.qe.microprofile.jwt.testapp.jaxrs.SecuredJaxRsEndpoint;
+import org.jboss.eap.qe.ts.common.docker.Docker;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
+
+/**
+ * Test a high level scenario which includes deploying a container with Keycloak, setting it up and obtaining
+ * authentication token from it.
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class KeycloakIntegrationHighLevelScenarioTest {
+
+    private static final String KEYCLOAK_REALM_NAME = "foobar";
+
+    private static final String KEYCLOAK_INSTANCE_HOSTNAME = "localhost";
+
+    private static final String KEYCLOAK_CONTAINER_NAME = KeycloakIntegrationHighLevelScenarioTest.class.getSimpleName()
+            + "-container";
+
+    private static final int KEYCLOAK_EXPOSED_HTTP_PORT = 8179;
+    private static final int KEYCLOAK_EXPOSED_TOKEN_PORT = 8149;
+    private static final int KEYCLOAK_EXPOSED_MANAGEMENT_PORT = 9988;
+
+    private static final String KEYCLOAK_ADMIN_USERNAME = "admin";
+    private static final String KEYCLOAK_ADMIN_PASSWORD = "password";
+
+    public static Docker keycloakContainer = new Docker.Builder(KEYCLOAK_CONTAINER_NAME,
+            "registry.hub.docker.com/jboss/keycloak:8.0.1")
+                    .setContainerReadyTimeout(2, TimeUnit.MINUTES)
+                    .setContainerReadyCondition(() -> isContainerReady(KEYCLOAK_EXPOSED_HTTP_PORT))
+                    .withPortMapping(KEYCLOAK_EXPOSED_HTTP_PORT + ":8080")
+                    .withPortMapping(KEYCLOAK_EXPOSED_TOKEN_PORT + ":8085")
+                    .withPortMapping(KEYCLOAK_EXPOSED_MANAGEMENT_PORT + ":9990")
+                    .withEnvVar("KEYCLOAK_USER", KEYCLOAK_ADMIN_USERNAME)
+                    .withEnvVar("KEYCLOAK_PASSWORD", KEYCLOAK_ADMIN_PASSWORD)
+                    .withCmdArg("-b=0.0.0.0")
+                    .withCmdArg("-bmanagement=0.0.0.0")
+                    .build();
+
+    public static KeycloakConfigurator keycloakConfigurator = new KeycloakConfigurator.Builder(KEYCLOAK_REALM_NAME)
+            .adminPassword(KEYCLOAK_ADMIN_PASSWORD)
+            .adminUsername(KEYCLOAK_ADMIN_USERNAME)
+            .clientId("client-custom-id")
+            .keycloakBindAddress(KEYCLOAK_INSTANCE_HOSTNAME)
+            .keycloakHttpPort(KEYCLOAK_EXPOSED_HTTP_PORT)
+            .build();
+
+    @ClassRule
+    public static TestRule ruleChain = RuleChain.outerRule(keycloakContainer)
+            .around(keycloakConfigurator);
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        //visit http://localhost:8179/auth/realms/foobar/.well-known/openid-configuration with running keycloak for values
+        final String mpProperties = "mp.jwt.verify.publickey.location=http://localhost:8179/auth/realms/%1$s/protocol/openid-connect/certs%n"
+                +
+                "mp.jwt.verify.issuer=http://localhost:8179/auth/realms/%1$s";
+
+        return ShrinkWrap.create(WebArchive.class, KeycloakIntegrationHighLevelScenarioTest.class.getSimpleName() + ".war")
+                .addClass(SecuredJaxRsEndpoint.class)
+                .addClass(JaxRsTestApplication.class)
+                .addAsManifestResource(
+                        new StringAsset(String.format(mpProperties, KEYCLOAK_REALM_NAME)),
+                        "microprofile-config.properties");
+    }
+
+    /**
+     * @tpTestDetails A high level scenario in which a Keycloak is used for generating a JWT which will be supplied to
+     *                server.
+     * @tpPassCrit Server accepts the JWT and returns its raw form.
+     * @tpSince EAP 7.4.0.CD19
+     */
+    @Test
+    public void accessSecuredEndpointWithKeycloakProvidedJwt(@ArquillianResource URL url) {
+        final String username = "qux";
+        final String password = "foo";
+        keycloakConfigurator.addUser(username, password);
+        final String rawToken = keycloakConfigurator.getRawJwtFromKeyCloak(username, password);
+
+        given().header("Authorization", "Bearer " + rawToken)
+                .when().get(url.toExternalForm() + Endpoints.SECURED_ENDPOINT)
+                .then()
+                .body(equalTo(rawToken))
+                .statusCode(200);
+    }
+
+    private static boolean isContainerReady(int port) {
+        try {
+            URL url = new URL("http://" + KEYCLOAK_INSTANCE_HOSTNAME + ":" + port + "/auth");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+}
