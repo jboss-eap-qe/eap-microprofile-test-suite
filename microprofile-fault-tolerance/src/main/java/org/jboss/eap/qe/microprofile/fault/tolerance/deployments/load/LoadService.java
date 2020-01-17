46867fa59194b3ec0f3322efa1ff8da95aadd577
Miroslav Novak <mnovak@redhat.com>
2020-01-17 10:44:59 +0100

    Add CPU load tests for MP FT

---
+++ microprofile-fault-tolerance/src/main/java/org/jboss/eap/qe/microprofile/fault/tolerance/deployments/load/LoadService.java
@@ -0,0 +1,40 @@
+package org.jboss.eap.qe.microprofile.fault.tolerance.deployments.load;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+
+/**
+ * Service with MP FT annotations to verify correct work under high CPU load.
+ */
+@ApplicationScoped
+public class LoadService {
+
+    @Timeout(5000)
+    public String timeout() throws InterruptedException {
+        Thread.sleep(10000);
+        return "Hello from @Timeout method";
+    }
+
+    /**
+     * Retry 10 times, takes ~2000 ms of retrying (as there is 200ms delay) in total before throwing Exception
+     *
+     */
+    @Retry(maxRetries = 10, delay = 200, retryOn = Exception.class)
+    public String retry() throws Exception {
+        throw new Exception("Exception from @Retry method.");
+    }
+
+    @Timeout(5000)
+    @Fallback(fallbackMethod = "fallback")
+    public String timeoutWithFallback() throws Exception {
+        Thread.sleep(10000);
+        return "Hello from @Timeout method";
+    }
+
+    public String fallback() {
+        return "Hello from @Fallback method";
+    }
+}
