3ee29a50b67a4cbcfdea20ff5375e17cded4f882
Sultan Zhantemirov <szhantem@redhat.com>
2020-09-09 11:13:46 +0200

    Move CustomConfig* tests to the separate Maven test execution

--- microprofile-metrics/src/test/java/org/jboss/eap/qe/microprofile/metrics/integration/config/CustomMetricCustomConfigSourceTest.java
+++ microprofile-metrics/src/test/java/org/jboss/eap/qe/microprofile/metrics/integration/config/CustomMetricCustomConfigSourceTest.java
@@ -69,21 +69,23 @@ public class CustomMetricCustomConfigSourceTest extends CustomMetricDynamicBaseT
         public void setup() throws Exception {
             try (OnlineManagementClient client = ManagementClientProvider.onlineStandalone()) {
                 client.execute(String.format("/system-property=%s:add(value=%s)", CustomConfigSource.FILEPATH_PROPERTY,
-                        CustomMetricCustomConfigSourceProviderTest.SetupTask.class.getResource(PROPERTY_FILENAME).getPath()));
+                        SetupTask.class.getResource(PROPERTY_FILENAME).getPath())).assertSuccess();
                 ModuleUtil.add(TEST_MODULE_NAME)
                         .setModuleXMLPath(SetupTask.class.getResource("configSourceModule.xml").getPath())
                         .addResource("config-source", CustomConfigSource.class)
                         .executeOn(client);
                 client.execute(String.format(
                         "/subsystem=microprofile-config-smallrye/config-source=cs-from-class:add(class={module=%s, name=%s})",
-                        TEST_MODULE_NAME, CustomConfigSource.class.getName()));
+                        TEST_MODULE_NAME, CustomConfigSource.class.getName())).assertSuccess();
             }
         }
 
         @Override
         public void tearDown() throws Exception {
             try (OnlineManagementClient client = ManagementClientProvider.onlineStandalone()) {
-                client.execute("/subsystem=microprofile-config-smallrye/config-source=cs-from-class:remove");
+                client.execute(String.format("/system-property=%s:remove", CustomConfigSource.FILEPATH_PROPERTY))
+                        .assertSuccess();
+                client.execute("/subsystem=microprofile-config-smallrye/config-source=cs-from-class:remove").assertSuccess();
                 ModuleUtil.remove(TEST_MODULE_NAME).executeOn(client);
             }
         }
