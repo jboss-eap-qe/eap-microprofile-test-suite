9efd68f7a67ccfa0c48177f4482c11946914dca6
Martin Choma <mchoma@10.0.0.96>
2020-09-11 09:06:27 +0200

    [WFLY-12342] Add server probes for readiness health check
    Changes relater to WFLY-12342:
    - adjust existing tests to work with new changes
    - added new tests for new fetures in WFLY-12342

--- microprofile-health/src/test/java/org/jboss/eap/qe/microprofile/health/integration/FailSafeCDISystemPropertyHealthTest.java
+++ microprofile-health/src/test/java/org/jboss/eap/qe/microprofile/health/integration/FailSafeCDISystemPropertyHealthTest.java
@@ -3,6 +3,7 @@ package org.jboss.eap.qe.microprofile.health.integration;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.arquillian.api.ServerSetup;
+import org.jboss.eap.qe.microprofile.health.DisableDefaultHealthProceduresSetupTask;
 import org.jboss.eap.qe.microprofile.tooling.server.configuration.arquillian.MicroProfileServerSetupTask;
 import org.jboss.eap.qe.microprofile.tooling.server.configuration.creaper.ManagementClientProvider;
 import org.junit.runner.RunWith;
@@ -16,7 +17,8 @@ import org.wildfly.extras.creaper.core.online.operations.admin.Administration;
  */
 @RunWith(Arquillian.class)
 @RunAsClient
-@ServerSetup({ MicroProfileFTSetupTask.class, FailSafeCDISystemPropertyHealthTest.SetupTask.class })
+@ServerSetup({ DisableDefaultHealthProceduresSetupTask.class, MicroProfileFTSetupTask.class,
+        FailSafeCDISystemPropertyHealthTest.SetupTask.class })
 public class FailSafeCDISystemPropertyHealthTest extends FailSafeCDIHealthBaseTest {
 
     @Override
