e7f805d786cfef5cde90004e8c7059ba293d6010
Jan Stourac <jstourac@redhat.com>
2021-01-28 21:39:04 +0100

    [EAP7-1592] Integrate MP Metrics 3.0 into EAP.
    Fix of tests in MP metrics module.
    
    Added explicit tags to the counted metric since default tags have
    been removed from the specification with move to MP Metrics 3.0.

--- microprofile-metrics/src/main/java/org/jboss/eap/qe/microprofile/metrics/namefellow/PingOneService.java
+++ microprofile-metrics/src/main/java/org/jboss/eap/qe/microprofile/metrics/namefellow/PingOneService.java
@@ -7,8 +7,10 @@ import org.eclipse.microprofile.metrics.annotation.Counted;
 @ApplicationScoped
 public class PingOneService {
     public static final String MESSAGE = "pong one";
+    public static final String PING_ONE_SERVICE_TAG = "ping-one-service-tag";
 
-    @Counted(name = "ping-count", absolute = true, displayName = "Pong Count", description = "Number of ping invocations")
+    @Counted(name = "ping-count", absolute = true, displayName = "Pong Count", description = "Number of ping invocations", tags = "_app="
+            + PING_ONE_SERVICE_TAG)
     public String ping() {
         return MESSAGE;
     }
--- microprofile-metrics/src/main/java/org/jboss/eap/qe/microprofile/metrics/namefellow/PingTwoService.java
+++ microprofile-metrics/src/main/java/org/jboss/eap/qe/microprofile/metrics/namefellow/PingTwoService.java
@@ -7,8 +7,10 @@ import org.eclipse.microprofile.metrics.annotation.Counted;
 @ApplicationScoped
 public class PingTwoService {
     public static final String MESSAGE = "pong two";
+    public static final String PING_TWO_SERVICE_TAG = "ping-two-service-tag";
 
-    @Counted(name = "ping-count", absolute = true, displayName = "Pong Count", description = "Number of ping invocations")
+    @Counted(name = "ping-count", absolute = true, displayName = "Pong Count", description = "Number of ping invocations", tags = "_app="
+            + PING_TWO_SERVICE_TAG)
     public String ping() {
         return MESSAGE;
     }
