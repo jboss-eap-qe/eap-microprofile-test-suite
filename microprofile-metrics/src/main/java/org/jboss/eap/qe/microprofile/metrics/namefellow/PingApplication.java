0b4dd2e77930878f326da7f4a1eb37a0d63c8a4a
Fabio Burzigotti <fburzigo@redhat.com>
2023-04-13 18:36:29 +0200

    [issue 274] - Removing Metrics and OpenTracing tests

--- microprofile-metrics/src/main/java/org/jboss/eap/qe/microprofile/metrics/namefellow/PingApplication.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.eap.qe.microprofile.metrics.namefellow;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class PingApplication extends Application {
-}
