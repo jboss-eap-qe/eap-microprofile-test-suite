24868c6dd20a11039ff223735e89db1a25999b1f
Fabio Burzigotti <fburzigo@redhat.com>
2023-04-19 13:49:18 +0200

    [issue 274] - MicroProfile Health integration: Removing the MetricsChecker class usage

--- microprofile-health/src/test/java/org/jboss/eap/qe/microprofile/health/integration/MicroProfileFTSetupTask.java
+++ microprofile-health/src/test/java/org/jboss/eap/qe/microprofile/health/integration/MicroProfileFTSetupTask.java
@@ -4,11 +4,7 @@ import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.EXT
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
 
 import org.jboss.as.controller.PathAddress;
-import org.jboss.as.controller.operations.common.Util;
 import org.jboss.eap.qe.microprofile.tooling.server.configuration.arquillian.MicroProfileServerSetupTask;
-import org.jboss.eap.qe.microprofile.tooling.server.configuration.creaper.ManagementClientProvider;
-import org.wildfly.extras.creaper.core.online.OnlineManagementClient;
-import org.wildfly.extras.creaper.core.online.operations.admin.Administration;
 
 /**
  * Setup server to provide support for MP FT.
@@ -22,19 +18,19 @@ public class MicroProfileFTSetupTask implements MicroProfileServerSetupTask {
 
     @Override
     public void setup() throws Exception {
-        try (OnlineManagementClient client = ManagementClientProvider.onlineStandalone()) {
-            client.execute(Util.createAddOperation(FT_EXTENSION_ADDRESS)).assertSuccess();
-            client.execute(Util.createAddOperation(FT_SUBSYSTEM_ADDRESS)).assertSuccess();
-            new Administration(client).reload();
-        }
+        //            try (OnlineManagementClient client = ManagementClientProvider.onlineStandalone()) {
+        //                client.execute(Util.createAddOperation(FT_EXTENSION_ADDRESS)).assertSuccess();
+        //                client.execute(Util.createAddOperation(FT_SUBSYSTEM_ADDRESS)).assertSuccess();
+        //                new Administration(client).reload();
+        //            }
     }
 
     @Override
     public void tearDown() throws Exception {
-        try (OnlineManagementClient client = ManagementClientProvider.onlineStandalone()) {
-            client.execute(Util.createRemoveOperation(FT_SUBSYSTEM_ADDRESS)).assertSuccess();
-            client.execute(Util.createRemoveOperation(FT_EXTENSION_ADDRESS)).assertSuccess();
-            new Administration(client).reload();
-        }
+        //            try (OnlineManagementClient client = ManagementClientProvider.onlineStandalone()) {
+        //                client.execute(Util.createRemoveOperation(FT_SUBSYSTEM_ADDRESS)).assertSuccess();
+        //                client.execute(Util.createRemoveOperation(FT_EXTENSION_ADDRESS)).assertSuccess();
+        //                new Administration(client).reload();
+        //            }
     }
 }
