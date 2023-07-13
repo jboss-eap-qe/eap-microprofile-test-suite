f54640905a8d71887a95fc96efffaa0fab390b49
Peter Mackay <pmackay@redhat.com>
2023-07-14 00:47:54 +0200

    Fix JWT tests after smallrye/smallrye-jwt#373

--- microprofile-jwt/src/test/java/org/jboss/eap/qe/microprofile/jwt/security/keyselection/MultipleConfiguredPublicKeysSelectionLocationPropTest.java
+++ microprofile-jwt/src/test/java/org/jboss/eap/qe/microprofile/jwt/security/keyselection/MultipleConfiguredPublicKeysSelectionLocationPropTest.java
@@ -117,8 +117,8 @@ public class MultipleConfiguredPublicKeysSelectionLocationPropTest {
      * @tpTestDetails A JWT signed by a "pink" private key is send to the server which has configured public key to be a
      *                JSON Web Key set in JSON. There are multiple keys in this set and there is no "pink" public key among
      *                them.
-     * @tpPassCrit JWt is rejected and user receives 401/forbidden because there is no matching configured public key on
-     *             the server.
+     * @tpPassCrit JWt is rejected and user receives 500/internal server error because there is no matching configured
+     *             public key on the server.
      * @tpSince EAP 7.4.0.CD19
      */
     @Test
@@ -129,7 +129,7 @@ public class MultipleConfiguredPublicKeysSelectionLocationPropTest {
         given().header("Authorization", "Bearer " + token.getRawValue())
                 .when().get(url.toExternalForm() + Endpoints.SECURED_ENDPOINT)
                 .then()
-                .statusCode(401);
+                .statusCode(500);
     }
 
     /**
@@ -176,8 +176,8 @@ public class MultipleConfiguredPublicKeysSelectionLocationPropTest {
      * @tpTestDetails A JWT signed by a "pink" private key is send to the server which has configured public key to be a
      *                JSON Web Key set Base64 encoded. There are multiple keys in this set and there is no "pink" public
      *                key among them.
-     * @tpPassCrit JWt is rejected and user receives 401/forbidden because there is no matching configured public key on
-     *             the server.
+     * @tpPassCrit JWt is rejected and user receives 500/internal server error because there is no matching configured
+     *             public key on the server.
      * @tpSince EAP 7.4.0.CD19
      */
     @Test
@@ -188,7 +188,7 @@ public class MultipleConfiguredPublicKeysSelectionLocationPropTest {
         given().header("Authorization", "Bearer " + token.getRawValue())
                 .when().get(url.toExternalForm() + Endpoints.SECURED_ENDPOINT)
                 .then()
-                .statusCode(401);
+                .statusCode(500);
     }
 
     private static URL getFileFromResources(final String filePath) {
