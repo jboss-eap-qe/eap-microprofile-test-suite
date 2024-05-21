8c4712a12f7f1876305be37c13b3e254035ed7b2
Fabio Burzigotti <fburzigo@redhat.com>
2024-05-21 17:02:03 +0200

    [Micrometer] - Configure deendency management (same as WildFly ATM), adding deployment resources

---
+++ micrometer/src/main/java/org/jboss/eap/qe/micrometer/multiple/TestApplication.java
@@ -0,0 +1,8 @@
+package org.jboss.eap.qe.micrometer.multiple;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class TestApplication extends Application {
+}
