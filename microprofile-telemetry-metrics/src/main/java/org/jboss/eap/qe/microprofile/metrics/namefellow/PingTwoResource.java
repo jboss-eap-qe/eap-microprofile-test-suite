dd872650f4517c4547cc925cc4d6d43d5ca88a3c
Marek Kopecky <mkopecky@redhat.com>
2024-11-27 14:15:19 +0100

    [WFLY-19846] Restore multideployment MP Metrics tests from #274 for MP Telemetry Metrics

---
+++ microprofile-telemetry-metrics/src/main/java/org/jboss/eap/qe/microprofile/metrics/namefellow/PingTwoResource.java
@@ -0,0 +1,21 @@
+package org.jboss.eap.qe.microprofile.metrics.namefellow;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/" + PingTwoResource.RESOURCE)
+public class PingTwoResource {
+    public static final String RESOURCE = "ping-one";
+
+    @Inject
+    private PingTwoService ping;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String doGet() {
+        return ping.ping();
+    }
+}
